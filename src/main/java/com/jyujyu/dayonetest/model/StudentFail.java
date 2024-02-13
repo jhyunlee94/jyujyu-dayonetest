b364a067db18102160ddebb62de8192583834429
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-13 16:26:12 +0900

    git pre commit hook 테스트

--- src/main/java/com/jyujyu/dayonetest/model/StudentFail.java
+++ src/main/java/com/jyujyu/dayonetest/model/StudentFail.java
@@ -2,26 +2,23 @@ package com.jyujyu.dayonetest.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+
+
 import lombok.Builder;
 import lombok.Getter;
+
+
 import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
-
-
-
-
-
 @NoArgsConstructor
 @Table(name = "student_fail")
 @Entity
 public class StudentFail {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "student_fail_id")
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "student_fail_id")
   private Long id;
 
   @Column(name = "exam")
