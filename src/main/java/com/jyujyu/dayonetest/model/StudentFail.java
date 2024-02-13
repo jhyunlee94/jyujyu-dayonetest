2f33cf20aef09d0bed9b54ef93c37d5fa84e98de
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-13 16:40:28 +0900

    git pre commit hook 테스트

--- src/main/java/com/jyujyu/dayonetest/model/StudentFail.java
+++ src/main/java/com/jyujyu/dayonetest/model/StudentFail.java
@@ -2,12 +2,10 @@ package com.jyujyu.dayonetest.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-
-
 import lombok.Builder;
+
+
 import lombok.Getter;
-
-
 import lombok.NoArgsConstructor;
 
 @Getter
