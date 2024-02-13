aacf9159498e134dcb949ff405744e7df6cdbab9
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-13 13:38:37 +0900

    Kafka Test 작성

--- src/test/java/com/jyujuy/dayonetest/IntegrationTest.java
+++ src/test/java/com/jyujuy/dayonetest/IntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
@@ -27,6 +28,7 @@ public class IntegrationTest {
     static DockerComposeContainer rdbms;
     static RedisContainer redis;
     static LocalStackContainer aws;
+    static KafkaContainer kafka;
 
     static {
         rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml")).withExposedService("local-db", 3306, Wait.forLogMessage(".*ready for connections.*", 1).withStartupTimeout(Duration.ofSeconds(300))).withExposedService("local-db-migrate", 0, Wait.forLogMessage("(.*Successfully applied.*)|(.*Successfully validated.*)", 1).withStartupTimeout(Duration.ofSeconds(300)));
@@ -40,6 +42,9 @@ public class IntegrationTest {
 //                .withServices(LocalStackContainer.Service.S3)
 //                .withStartupTimeout(Duration.ofSeconds(600));
 //        aws.start();
+        kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"))
+                .withKraft();
+        kafka.start();
     }
 
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -70,6 +75,8 @@ public class IntegrationTest {
             } catch (Exception e) {
                 // ignore
             }
+
+            properties.put("spring.kafka.bootstrap-servers", kafka.getBootstrapServers());
 
             TestPropertyValues.of(properties)
                     .applyTo(applicationContext);
