13ab7bc9b306385c9bf29697736901587ecde21f
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-13 10:28:03 +0900

    IntegrationTest: 클래스 추가 / RDBMS testcontainers 추가

---
+++ src/test/java/com/jyujuy/dayonetest/IntegrationTest.java
@@ -0,0 +1,45 @@
+package com.jyujuy.dayonetest;
+
+import org.junit.Ignore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Ignore // 만들고 있는 클래스는 다른 클래스에 상속시킬 부모 클래스이기에 ignore 를 붙여줍니다.
+@Transactional // 테스트가 진행된 다음에 롤백되기때문에 트랜잭션 설정해줍니다.
+@SpringBootTest // 실제로 스프링 실행하는거처럼 빈들을 다 적용해줍니다.
+@ContextConfiguration(initializers = IntegrationTest.IntegrationTestInitializer.class) // 아래가 동작할려면 이와같이 넣어줘야 합니다.
+public class IntegrationTest {
+
+    static DockerComposeContainer rdbms;
+
+    static {
+        rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml")).withExposedService("local-db", 3306, Wait.forLogMessage(".*ready for connections.*", 1).withStartupTimeout(Duration.ofSeconds(300))).withExposedService("local-db-migrate", 0, Wait.forLogMessage("(.*Successfully applied.*)|(.*Successfully validated.*)", 1).withStartupTimeout(Duration.ofSeconds(300)));
+        rdbms.start();
+    }
+
+    static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            Map<String, String> properties = new HashMap<>();
+
+            var rdbmsHost = rdbms.getServiceHost("local-db", 3306);
+            var rdbmsPort = rdbms.getServicePort("local-db", 3306);
+
+            properties.put("spring.datasource.url", "jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort + "/score");
+
+            TestPropertyValues.of(properties)
+                    .applyTo(applicationContext);
+        }
+    }
+}
